097a31bd2cbe44e6f5decf18a3c32d81b81aa593
Adam Land <adam@tokensoft.io>
2023-10-17 08:36:01 -0400

    add foundry plugin

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -9,6 +9,8 @@ require("@typechain/hardhat");
 // this allows hardhat to use ethers for tests
 require("@nomiclabs/hardhat-ethers");
 
+require("@nomicfoundation/hardhat-foundry");
+
 // allow upgradeable contracts in tests
 // require('@openzeppelin/hardhat-upgrades');
 
