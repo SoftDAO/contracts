1d2441a8aa542de4e4176fe55c53a59f52b49e96
Jack Dishman <me@jackdishman.com>
2024-02-06 15:00:08 -0500

    feat: add template and init eventHandler file

---
+++ packages/subgraph/src/distributor/v3.0/continuousVestingMerkleDistributor.ts
@@ -0,0 +1,12 @@
+// - event: decreaseAllowance(address,uint256)
+// handler: handleDecreaseAllowance
+// - event: increaseAllowance(address,uint256)
+// handler: handleIncreaseAllowance
+// - event: setMerkleRoot(bytes32)
+// handler: handleSetMerkleRoot
+// - event: setUri(string)
+// handler: handleSetUri
+// - event: setVestingConfig(uint256,uint256,uint256)
+// handler: handleSetVestingConfig
+
+import { log } from '@graphprotocol/graph-ts'
