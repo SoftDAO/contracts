4f7756e367d9f3c838d4a1a0f108aa0c4da8e0da
Jack Dishman <me@jackdishman.com>
2024-03-22 12:12:46 -0400

    feat: fix scripts for deployment

--- packages/subgraph/scripts/abi_publish.ts
+++ packages/subgraph/scripts/abi_publish.ts
@@ -54,7 +54,10 @@ async function main() {
   console.log("✅  Published contracts to the subgraph package.");
 }
 main()
-  .then(() => process.exit(0))
+  .then(() => {
+    console.log(`exiting process`)
+    process.exit(0)
+  })
   .catch((error) => {
     console.error(error);
     process.exit(1);
