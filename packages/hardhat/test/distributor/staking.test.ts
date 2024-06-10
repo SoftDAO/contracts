1987e4aff610d2b6bd5f768dcf48b011066f9a16
Tenor <tenor@fmail.co.uk>
2024-06-10 18:09:45 -0400

    include penalized and non-penalized unstaking in staking tests

--- packages/hardhat/test/distributor/staking.test.ts
+++ packages/hardhat/test/distributor/staking.test.ts
@@ -1,6 +1,7 @@
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { GenericERC20, StakingContract, SoftMfersMock } from "../../typechain-types";
 import { ethers } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 jest.setTimeout(30000);
 
@@ -13,8 +14,27 @@ let tokenAddress: string;
 let softMfersContractAddress: string;
 let stakingContractAddress: string;
 
+// 2500  15 days	10%
+// 5000  30 Days	10%
+// 10000 30 Days	5%
+// 25000 45 Days	5%
+// 50000 60 Days	2%
+// 75000 90 Days	2%
+const spec = [
+  {
+    maximumAmount: BigInt(10000e18) - BigInt(1),
+    maximumPenaltyPeriod: 86400 * 30 - 1,
+    expectedPenalty: 0.1,
+  },
+  {
+    maximumAmount: BigInt(25000e18) - BigInt(1),
+    maximumPenaltyPeriod: 86400 * 30 - 1,
+    expectedPenalty: 0.05,
+  },
+];
+
 describe("StakingContract", function () {
-  beforeAll(async () => {
+  beforeEach(async () => {
     [deployer, user1] = await ethers.getSigners();
 
     const GenericERC20Factory = await ethers.getContractFactory("GenericERC20", deployer);
@@ -46,10 +66,26 @@ describe("StakingContract", function () {
     expect(stakedAmount).toEqual(BigInt(stakeAmount));
   });
 
-  it("should allow users to unstake tokens", async () => {
+  it("should incur penalty for withdrawal", async () => {
     const stakeAmount = 500;
+    await token.connect(user1).approve(stakingContractAddress, stakeAmount);
+    await stakingContract.connect(user1).stake(stakeAmount, tokenAddress);
+
+    await time.increase(86400 * 30 - 30);
     await stakingContract.connect(user1).unstake(stakeAmount, tokenAddress);
 
+    expect(await token.balanceOf(user1.address)).toEqual(BigInt(stakeAmount * 0.9));
+    const [stakedAmount] = await stakingContract.stakedTokens(user1.address);
+    expect(stakedAmount).toEqual(BigInt(0));
+  });
+
+  it("should not incur penalty for withdrawal after penalty period passes", async () => {
+    const stakeAmount = 500;
+
+    await token.connect(user1).approve(stakingContractAddress, stakeAmount);
+    await stakingContract.connect(user1).stake(stakeAmount, tokenAddress);
+    await time.increase(86400 * 30);
+    await stakingContract.connect(user1).unstake(stakeAmount, tokenAddress);
     expect(await token.balanceOf(user1.address)).toEqual(BigInt(stakeAmount));
     const [stakedAmount] = await stakingContract.stakedTokens(user1.address);
     expect(stakedAmount).toEqual(BigInt(0));
