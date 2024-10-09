0109c6a9dec3eefee7a789d6d765268d26715987
ArshaanB <arshaan.bhimani@uwaterloo.ca>
2024-10-08 22:09:41 -0400

    setup and deployed subgraph to base sepolia

---
+++ packages/subgraph/src/sale/v4.0/saleImplementationMapping.ts
@@ -0,0 +1,32 @@
+import {
+  ImplementationConstructor,
+  FlatPriceSale_v_4_0 as FlatPriceSaleContract
+} from "../../../generated/FlatPriceSale_v_4_0/FlatPriceSale_v_4_0";
+import { SaleImplementation } from "../../../generated/schema";
+import { log, BigInt } from "@graphprotocol/graph-ts";
+
+export function handleImplementationConstructor(
+  event: ImplementationConstructor
+): void {
+  const saleImplementationId = event.address.toHexString();
+
+  const implementation = new SaleImplementation(saleImplementationId);
+  const implementationContract = FlatPriceSaleContract.bind(event.address);
+
+  implementation.networkConfig = event.params.networkConfig.toHexString();
+  implementation.saleCount = BigInt.fromI32(0);
+  implementation.purchaseCount = BigInt.fromI32(0);
+  const versionResult = implementationContract.try_VERSION();
+
+  if (versionResult.reverted) {
+    log.error("could not call VERSION() on implementation at {} in tx {}", [
+      saleImplementationId,
+      event.transaction.hash.toHexString()
+    ]);
+    return;
+  }
+
+  implementation.version = versionResult.value;
+  implementation.createdAt = event.block.timestamp;
+  implementation.save();
+}
