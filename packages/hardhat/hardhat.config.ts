cdf5aefadf6a6d5c282170752a55d8821552d6ba
ArshaanB <arshaan.bhimani@uwaterloo.ca>
2024-12-29 22:31:58 -0500

    remove text

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -9,11 +9,11 @@
 // import "hardhat-deploy";
 // import "hardhat-deploy-ethers";
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-ignition-ethers";
+import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-jest"; // Typescript
+import { HardhatUserConfig } from "hardhat/config";
 
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
@@ -50,12 +50,12 @@ export default config;
 
 // // If not set, it uses ours Alchemy's default API key.
 // // You can get your own at https://dashboard.alchemyapi.io
-// const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+// const providerApiKey = process.env.ALCHEMY_API_KEY || "XXX";
 // // If not set, it uses the hardhat account 0 private key.
 // const deployerPrivateKey =
-//   process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+//   process.env.DEPLOYER_PRIVATE_KEY ?? "0x000";
 // // If not set, it uses ours Etherscan default API key.
-// const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+// const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "XXX";
 
 // const selectedNetwork = process.env.HARDHAT_NETWORK || process.env.NETWORK || "localhost";
 
